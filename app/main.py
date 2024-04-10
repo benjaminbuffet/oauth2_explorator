1eeb11675c499170fb2ff352ab55f1b7ced7ac5d
gibelot <b.buffet@lexfo.fr>
2024-04-10 17:54:43 +0200

    feat: :sparkles: Mains features explorer and usage sample

---
+++ app/main.py
@@ -0,0 +1,80 @@
+import streamlit as st
+import requests
+import json
+import urllib.parse
+import jwt
+
+tab1, tab2 = st.tabs(["auth", "well-known"])
+
+with tab1:
+    st.header("Configuration", divider="grey")
+    st.json(json.dumps(dict(st.secrets.kc)))
+
+    st.header("Step 1", divider="grey")
+    first_step_url = (
+        f"{st.secrets.kc.base_url}"
+        + f"/realms/{st.secrets.kc.realm}/protocol/openid-connect/auth?"
+        + f"client_id={st.secrets.kc.client_id}&"
+        + f"redirect_uri={urllib.parse.quote(st.secrets.kc.redirect_uri, safe='')}&"
+        + "response_type=code&scope=openid"
+    )
+    st.text(first_step_url.replace("&", "&\n").replace("?", "?\n"))
+    st.html(
+        f'<a href="{first_step_url}" target="_self">Click here to connect</a>'
+    )
+
+    if "step" in st.query_params and st.query_params["step"] == "redirect":
+
+        st.header("Step 2", divider="grey")
+
+        st.write(st.query_params)
+
+        st.header("Step 3", divider="grey")
+
+        third_step_url = (
+            f"{st.secrets.kc.base_url}"
+            + f"/realms/{st.secrets.kc.realm}/protocol/openid-connect/token"
+        )
+
+        body = {
+            "client_id": st.secrets.kc.client_id,
+            "redirect_uri": st.secrets.kc.redirect_uri,
+            "grant_type": "authorization_code",
+            "code": st.query_params["code"],
+        }
+        st.subheader("Post request")
+        st.write(third_step_url)
+        st.write(body)
+
+        st.subheader("Response")
+        response = requests.post(third_step_url, data=body)
+        json = response.json()
+        st.write(json)
+
+        if "access_token" in json:
+            st.subheader("access_token")
+            st.write(
+                jwt.decode(
+                    json["access_token"], options={"verify_signature": False}
+                )
+            )
+
+        if "id_token" in json:
+            st.subheader("id_token")
+            st.write(
+                jwt.decode(
+                    json["id_token"], options={"verify_signature": False}
+                )
+            )
+
+
+with tab2:
+    well_know_url = (
+        f"{st.secrets.kc.base_url}"
+        + f"/realms/{st.secrets.kc.realm}/"
+        + ".well-known/openid-configuration"
+    )
+    response = requests.get(well_know_url)
+    st.write(well_know_url)
+    st.divider()
+    st.write(response.json())
---
+++ app/pages/private.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import requests
+import jwt
+import urllib.parse
+
+redirect_uri = "http://localhost:8501/private"
+
+def is_logged_in():
+    if (
+        "id_token" in st.session_state
+        and st.session_state.id_token is not None
+    ):
+        return True
+    return False
+
+
+if "code" in st.query_params:
+    third_step_url = (
+        f"{st.secrets.kc.base_url}"
+        + f"/realms/{st.secrets.kc.realm}/protocol/openid-connect/token"
+    )
+
+    body = {
+        "client_id": st.secrets.kc.client_id,
+        "redirect_uri": redirect_uri,
+        "grant_type": "authorization_code",
+        "code": st.query_params["code"],
+    }
+
+    response = requests.post(third_step_url, data=body)
+    json = response.json()
+
+    if "id_token" in json:
+        st.session_state.id_token = json["id_token"]
+        id_token_decoded = jwt.decode(
+            json["id_token"], options={"verify_signature": False}
+        )
+
+        st.session_state.id_token = json["id_token"]
+        st.session_state.name = id_token_decoded["name"]
+
+
+if not is_logged_in():
+    redirect_uri = "http://localhost:8501/private"
+    first_step_url = (
+        f"{st.secrets.kc.base_url}"
+        + f"/realms/{st.secrets.kc.realm}/protocol/openid-connect/auth?"
+        + f"client_id={st.secrets.kc.client_id}&"
+        + f"redirect_uri={urllib.parse.quote(redirect_uri, safe='')}&"
+        + "response_type=code&scope=openid"
+    )
+
+    st.html(
+        f'<a href="{first_step_url}" target="_self">Click here to connect</a>'
+    )
+
+else:
+    st.header(f"Hello, {st.session_state.name}", divider="grey")
+    st.text("This page is private")
